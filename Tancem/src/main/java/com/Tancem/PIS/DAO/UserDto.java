dfb1ddc9853ecb57d9bf10db2b1db5160e276c96
VISHNUVARDHAN25012002 <116712471+VISHNUVARDHAN25012002@users.noreply.github.com>
2024-07-29 14:11:30 +0530

    test file

--- Tancem/src/main/java/com/Tancem/PIS/DAO/UserDto.java
+++ Tancem/src/main/java/com/Tancem/PIS/DAO/UserDto.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class UserDto {
-    private String emp_id ;
+    private String emp_id;
     private String password;
     private String role;
 }
---
+++ Tancem/src/main/java/com/Tancem/PIS/config/Hello.java
@@ -0,0 +1,7 @@
+package com.Tancem.PIS.config;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
